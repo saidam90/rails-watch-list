4daa4fefa95ef9134d15b2c889215fdfe8a23219
Saida Murtazali <saida.murtazaliyeva@yahoo.com>
2022-11-18 14:50:33 +0000

    done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,7 +19,7 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark.destroy
-    redirect_to lists_path, status: :see_other
+    redirect_to lists_path(@bookmark.list), status: :see_other
   end
 
   private
